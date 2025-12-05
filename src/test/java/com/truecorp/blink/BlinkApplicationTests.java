a3c7f82c04686fd15ceb9eb4004c91c15c173312
Varuzhan <varuzhan.true@gmail.com>
2025-12-05 21:02:05 +0400

    chore: Commit remaining structural files

---
+++ src/test/java/com/truecorp/blink/BlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truecorp.blink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
